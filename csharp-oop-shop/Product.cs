9a1fbb72898409987979a257147cd993baa1d147
Brody14 <stefania.spinas@gmail.com>
2023-09-08 17:00:02 +0200

    Esercizio completato

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+
+    /*creare la classe Prodotto che gestisce i prodotti di uno shop. Un prodotto è caratterizzato da:
+           codice(numero intero)
+           nome
+           descrizione
+           prezzo
+           iva
+    Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di 
+    “utilità” per fare in modo che:
+    alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+    Il codice prodotto sia accessibile solo in lettura
+    Gli altri attributi siano accessibili sia in lettura che in scrittura
+  */
+    public class Product
+    {
+        //ATTRIBUTI
+        private int code;
+        public string name { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public int vat { get; set; }
+
+        //COSTRUTTORE
+        public Product(string name, string description, double price, int vat)
+        {
+            SetCode();
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.vat = vat;
+        }
+
+        //GETTER
+
+        public int GetCode()
+        {
+            return code;
+        }
+
+        //SETTER
+        private void SetCode()
+        {
+            Random rnd = new Random();
+            this.code = rnd.Next(1,500);
+           
+        }
+
+        //METODI
+        /*Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+         */
+
+        public double PriceWithTax()
+        {
+            return price + (price * vat) / 100;
+        }
+
+        /*Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome*/
+
+        public string ProductFullName()
+        {
+            return $"{code} {name}";
+        }
+
+       
+    }
+}
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Product beer = new Product("IPA", "Indubbiamente la migliore", 10, 22);
+            Console.WriteLine($"Nome prodotto: {beer.name}, descrizione: {beer.description}, prezzo: {beer.price}€, IVA: {beer.vat}%, codice prodotto: {beer.GetCode()}");
+            Console.WriteLine($"Il prezzo comprensivo di IVA del prodotto {beer.name} è: {beer.PriceWithTax()}€");
+            Console.WriteLine($"Il nome completo del prodotto è: {beer.ProductFullName()}");
+
         }
     }
 }
