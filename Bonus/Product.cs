71cc86c2f3caa513d7a9829017d0c94fe0b45bd4
Brody14 <stefania.spinas@gmail.com>
2023-09-08 17:43:58 +0200

    FEAT Bonus pt.1

---
+++ Bonus/Product.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus
+{
+    public class Product
+    {
+        //ATTRIBUTI
+        private int code;
+        public string name { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public int vat { get; set; }
+
+        //COSTRUTTORE
+        public Product(string name, string description, double price, int vat)
+        {
+            SetCode();
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.vat = vat;
+        }
+
+        //GETTER
+
+        public int GetCode()
+        {
+            return code;
+        }
+
+        //SETTER
+        private void SetCode()
+        {
+            Random rnd = new Random();
+            this.code = rnd.Next(1, 500);
+
+        }
+
+        //METODI
+        /*Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+         */
+
+        public double PriceWithTax()
+        {
+            return price + (price * vat) / 100;
+        }
+
+        /*Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome*/
+
+        public string ProductFullName()
+        {
+            return $"{code} {name}";
+        }
+
+        /*create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 
+         * 00000091, mentre codice 12344556 resta come è)*/
+        public string CodeExtended()
+        {
+            string codeString = code.ToString();
+
+            for (int i = codeString.Length; i < 8; i++)
+            {
+                codeString = 0 + codeString;
+            }
+
+            return codeString;
+        }
+
+        
+        }
+
+}
+
+
+
---
+++ Bonus/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Product beer = new Product("IPA", "Indubbiamente la migliore", 10, 22);
+            Console.WriteLine($"Nome prodotto: {beer.name}, descrizione: {beer.description}, prezzo: {beer.price}€, IVA: {beer.vat}%, codice prodotto: {beer.GetCode()}");
+            Console.WriteLine($"Il prezzo comprensivo di IVA del prodotto {beer.name} è: {beer.PriceWithTax()}€");
+            Console.WriteLine($"Il nome completo del prodotto è: {beer.ProductFullName()}");
+            Console.WriteLine($"Codice con pad left: {beer.CodeExtended()}");           
+        }
+    }
+}
